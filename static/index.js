420644c7096c7b7a1ab3014c19fe3618a9e27d4c
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-01-26 20:59:29 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -401,11 +401,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
         // TODO 添加反馈
         if (mode === MODE_ENDLESS) return '';
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '就这？';
+        if (score <= 99) return '我用舌头打都比你强';
+        if (score <= 149) return '马达马达';
+        if (score <= 199) return '快赶上我了';
+        return '爹，教教我！';
     }
 
     function toStr(obj) {
